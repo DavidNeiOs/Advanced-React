79c198f8c9c498aa96210251c03e870640d07ac3
DavidNeiOs <davidneios25@gmail.com>
2018-11-10 19:35:25 -0500

    ep 30

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,17 +1,34 @@
-require('dotenv').config({ path: 'variables.env'});
-const createServer = require('./createServer');
-const db = require('./db');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
-// Todo use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // Todo use express middleware to populate current user
 
-server.start({
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    }
-}, deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`)
+//decode the jwt so we can get the user id in each request
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  //console.log(token);
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userid onto the request for future requests to access
+    req.userId = userId;
+  }
+  next();
 });
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  }
+);
